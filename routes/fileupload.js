50a9f654a114bbb83c1d9a59d7e82487a9eaabf7
Aqib Asif <AqibAsif@AQIBs-Macbook.local>
2020-11-12 21:46:45 +0500

    File upload added

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -4,56 +4,56 @@ const multer = require('multer');
 const app = express();
 const router = express.Router();
 
-// app.use(express.static(__dirname + '../../Beverix-app/fyp/public/uploads'));
+app.use(express.static(__dirname + '../../Beverix-app/fyp/public/uploads'));
 
-// var storage = multer.diskStorage({
-//     destination: '../../Beverix-app/fyp/public/uploads',
+var storage = multer.diskStorage({
+    destination: '../../Beverix-app/fyp/public/uploads',
   
-//     filename: function (req, file, callback) {
+    filename: function (req, file, callback) {
   
-//       callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-//     }
-//   })
+      callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  })
 
-// // const storage = multer.diskStorage({
-// //   destination: '../../Beverix-app/fyp/public/uploads',
-// //   //         function (req, file, cb) {
-// //   //     cb(null, path.resolve(__dirname,'../../fyp/public/uploads'));
-// //   //   },
-// //   filename: function (req, file, cb) {
-// //     cb(null, file.originalname);
-// //   },
-// // });
-
-// // var upload = multer({ storage: storage });
-
-// const fileFilter = (req, file, cb) => {
-//   // reject a file
-//   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-//     cb(null, true);
-//   } else {
-//     cb(null, false);
-//   }
-// };
-
-// const upload = multer({
-//   storage: storage,
-//   limits: {
-//     fileSize: 1024 * 1024 * 5,
+// const storage = multer.diskStorage({
+//   destination: '../../Beverix-app/fyp/public/uploads',
+//   //         function (req, file, cb) {
+//   //     cb(null, path.resolve(__dirname,'../../fyp/public/uploads'));
+//   //   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
 //   },
-//   fileFilter: fileFilter,
 // });
 
+// var upload = multer({ storage: storage });
 
-// router.post('/',  upload.single('photo'),  (req, res, next) => {
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
+
+
+router.post('/',  upload.single('photo'),  (req, res, next) => {
  
-//   console.log(req.file.path.substr(28));
-// //   return  res.json({
-// //     image: req.file.path,
-// //   });
+  console.log(req.file.path.substr(28));
+//   return  res.json({
+//     image: req.file.path,
+//   });
 
-//     const image = req.file.path.substr(28);
-//     res.send(image);
-// });
+    const image = req.file.path.substr(28);
+    res.send(image);
+});
 
 module.exports = router;
