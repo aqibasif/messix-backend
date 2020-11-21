46612851a760d10db8382c4c197f6d293da171c3
Aqib Asif <AqibAsif@AQIBs-Macbook.local>
2020-11-21 12:23:55 +0500

    New link

--- routes/users.js
+++ routes/users.js
@@ -133,7 +133,7 @@ router.put('/:id', auth, async (req, res) => {
 });
 
 router.post('/forgotpasswordlink/:email', async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://beverix.herokuapp.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
