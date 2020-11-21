9f76f6e160a9ce04056aea41f4f3c2b96e277690
Aqib Asif <AqibAsif@AQIBs-Macbook.local>
2020-11-21 13:30:28 +0500

    New link

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ const express = require('express');
 const config = require('config');
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
 require('./startup/logging')();
 require('./startup/cors')(app);
 require('./startup/routes')(app);
@@ -21,6 +10,15 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://beverix.herokuapp.com'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 const port = process.env.PORT || config.get('port');
 const server = app.listen(port, () =>
