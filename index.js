23764976ab0e9d2aa671ec229f444cbbf73445ac
Aqib Asif <AqibAsif@AQIBs-Macbook.local>
2020-11-21 15:26:13 +0500

    New

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require('express');
 const config = require('config');
 const app = express();
 
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 require('./startup/logging')();
 require('./startup/cors')(app);
