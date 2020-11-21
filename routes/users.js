47154a390fa8578524d4202b4479350d38922809
Aqib Asif <AqibAsif@AQIBs-Macbook.local>
2020-11-21 12:27:32 +0500

    New link

--- routes/users.js
+++ routes/users.js
@@ -132,11 +132,19 @@ router.put('/:id', auth, async (req, res) => {
     .send(user);
 });
 
+router.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 router.post('/forgotpasswordlink/:email', async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://beverix.herokuapp.com/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+  // res.setHeader('Access-Control-Allow-Origin', 'https://beverix.herokuapp.com/');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  // res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
   if (req.params.email === '') {
     res.status(400).send('email required');
