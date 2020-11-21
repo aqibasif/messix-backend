d1bb3929092d27750eb6e2146bbfb9b4d3d49196
Aqib Asif <AqibAsif@AQIBs-Macbook.local>
2020-11-21 13:14:18 +0500

    New link

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,13 @@ const keys = require('../config/dev');
 var app = express();
 var cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
